0c42b5c200f13b4dfb7d99dbac8fcc4c736be3dc
Joel Gombin <joel.gombin@gmail.com>
2016-11-16 17:01:05 +0100

    fin jour 6

--- script_carto.R
+++ script_carto.R
@@ -5,14 +5,22 @@ library(spdplyr)
 accidents <- read_shape("./data/accidentologie-paris.shp")
 
 accidents <- accidents %>% 
-  mutate(velo = ifelse(vehicule_1_ == "Bicy" | vehicule_2_ == "Bicy" | vehicule_3_ == "Bicy",
+  mutate(velo = ifelse(vehicule_1_ %in% "Bicy" | vehicule_2_ %in% "Bicy" | vehicule_3_ %in% "Bicy",
                        "bicy",
                        "autres")) %>% 
   filter(!is.na(velo))
 
 paris <- read_osm(bbox(accidents))
 
-tm_shape(paris) +
+carte <- tm_shape(paris) +
   tm_raster() +
 tm_shape(accidents) +
   tm_bubbles(size = 0.2, col = "velo", palette = "Set1")
+
+tmap_mode("view")
+
+carte
+
+tmap_mode("plot")
+
+carte
